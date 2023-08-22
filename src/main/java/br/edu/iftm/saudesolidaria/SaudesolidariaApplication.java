8d0fcdbdf75d2c121d10f1d0b64c429c7a502b5e
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2023-08-22 08:30:31 -0300

    feat initial commit

---
+++ src/main/java/br/edu/iftm/saudesolidaria/SaudesolidariaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.saudesolidaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudesolidariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudesolidariaApplication.class, args);
+	}
+
+}
